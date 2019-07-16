4f75799d0ec6e8c097c7ba0c9e2e95d249b57378
Zach Hirschman <zachhirschman@gmail.com>
2019-07-16 10:51:16 -0700

    auto commit from server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var shell = require("shelljs")
 
 app.get("/api/test-shell", (req,res,next) =>{
     shell.echo('Accessing the shell now')
-    shell.exec('git remote add origin https://github.com/zachhirschman/auto-commit-project.git')
+    // shell.exec('git remote add origin https://github.com/zachhirschman/auto-commit-project.git')
     shell.exec('git add .')
     shell.exec('git status')
     shell.exec('git commit -m "auto commit from server"')
